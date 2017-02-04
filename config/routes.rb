c8c5cab621a692c9c251ca1b50e7d0bb04a2dc48
rbrunoleal <@7v266roberto>
2017-02-04 11:09:45 -0300

    Adicionado scaffold Unidades

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :unidades
-  resources :areas
+
+  resources :areas do
+    resources :unidades
+  end
+
   root to: redirect('/areas')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
