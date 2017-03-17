b8e8f763dbb7fc08ef589147d187d664e9aaae1e
Katharine Schramm <katharineschramm@gmail.com>
2017-03-17 22:58:20 +0000

    Colocando de volta controller Unidades

--- app/controllers/unidades_controller.rb
+++ app/controllers/unidades_controller.rb
@@ -1,5 +1,5 @@
 class UnidadesController < ApplicationController
-  before_action :set_unidade, only: [:show]
+  before_action :set_unidade, only: [:show, :edit, :update, :destroy]
 
   # GET /unidades
   # GET /unidades.json2
@@ -22,7 +22,53 @@ class UnidadesController < ApplicationController
   end
 
   # GET /unidades/new
- 
+  def new
+    @unidade = Unidade.new
+  end
+
+  # GET /unidades/1/edit
+  def edit
+  end
+
+  # POST /unidades
+  # POST /unidades.json
+  def create
+    @unidade = Unidade.new(unidade_params)
+
+    respond_to do |format|
+      if @unidade.save
+        format.html { redirect_to @unidade, notice: 'Unidade was successfully created.' }
+        format.json { render :show, status: :created, location: @unidade }
+      else
+        format.html { render :new }
+        format.json { render json: @unidade.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /unidades/1
+  # PATCH/PUT /unidades/1.json
+  def update
+    respond_to do |format|
+      if @unidade.update(unidade_params)
+        format.html { redirect_to @unidade, notice: 'Unidade was successfully updated.' }
+        format.json { render :show, status: :ok, location: @unidade }
+      else
+        format.html { render :edit }
+        format.json { render json: @unidade.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /unidades/1
+  # DELETE /unidades/1.json
+  def destroy
+    @unidade.destroy
+    respond_to do |format|
+      format.html { redirect_to unidades_url, notice: 'Unidade was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -35,3 +81,4 @@ class UnidadesController < ApplicationController
       params.require(:unidade).permit(:descricao, :area_id)
     end
 end
+
