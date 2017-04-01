e7b397cc960067bd7b22bcd5e680bc8a4f45cc5e
Katharine Schramm <katharineschramm@gmail.com>
2017-04-01 02:05:22 +0000

    Travis

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,9 +1,6 @@
 RailsAdmin.config do |config|
   
-  config.authorize_with do
-    redirect_to main_app.root_path unless current_user.role == admin
-  end
-
+  
   ### Popular gems integration
 
   ## == Devise ==
