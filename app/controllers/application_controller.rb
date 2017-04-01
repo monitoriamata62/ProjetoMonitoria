d5bc208c9ebb39db573d59c26f84b56b6021ceae
Katharine Schramm <katharineschramm@gmail.com>
2017-04-01 06:19:32 +0000

    Tirando a opção de escolher role no cadastro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation, :role])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
   end
 
 
