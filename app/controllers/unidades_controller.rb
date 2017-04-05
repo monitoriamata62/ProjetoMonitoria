22eee76eab03b3b84f6656455af1f096f12af3ac
Weslley <ley_silva@hotmail.com>
2017-04-05 22:39:17 +0000

    Removendo Paginação

--- app/controllers/unidades_controller.rb
+++ app/controllers/unidades_controller.rb
@@ -11,7 +11,7 @@ class UnidadesController < ApplicationController
       if params[:search]
         @unidades = Unidade.where("descricao like ?", "%#{params[:search]}%")
       else
-        @unidades = Unidade.order(:descricao).page(params['page']).per(10)
+        @unidades = Unidade.order(:descricao)
       end
     end
   end
